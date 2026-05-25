10265aae9c5149181b299bbe1e72302aaa281eed
shubhpid <shubhpid@localhost>
2026-05-25 04:47:43 +0400

    word counter py function

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greeting boots")
+def get_book_text(filepath: str) -> str:
+    with open(filepath, "r") as file:
+        return file.read()
+    
+def num_words(text: str) -> str:
+    words = text.split()
+    return str(len(words))
+
+
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    print(f"Found {num_words(book_text)} total words")
+
+
+main()
